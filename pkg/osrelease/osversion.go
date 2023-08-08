2e209f96c1444c791ca6d58767308ca35f205e94
christa <christa0584@gmail.com>
2023-08-08 18:57:54 +1000

    perf: add the date of kernel compiling checking in kernel checking

--- pkg/osrelease/osversion.go
+++ pkg/osrelease/osversion.go
@@ -1,8 +1,15 @@
 package osrelease
 
+import "time"
+
 type OsVersion struct {
 	NAME       string `json:"name"`
 	OID        string `json:"oid"`
 	VERSION    string `json:"version"`
 	VERSION_ID string `json:"version___id"`
+}
+
+type KernelVersion struct {
+	Version   string
+	BuiltDate time.Time
 }
